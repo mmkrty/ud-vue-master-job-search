97daa5e0cd496b56ffecd722d748e372f4f1113a
Hao Chen <mmkrty@gmail.com>
2023-09-08 14:51:52 +0200

    style main nav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,15 @@
 <template>
-  <header class="w-full text-sm">Main Nav</header>
+  <header class="w-full text-sm">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div
+        class="border-brand-gray-1 mx-auto flex h-full flex-nowrap border-b border-solid px-8"
+      >
+        <a href="/" class="flex h-full items-center text-xl font-semibold"
+          >Hao Careers</a
+        >
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
