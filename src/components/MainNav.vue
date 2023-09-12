9f0c41a2783f1d88a0ffd72360cc33ca77e37af6
Hao Chen <mmkrty@gmail.com>
2023-09-12 16:55:12 +0200

    add nav list

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -7,6 +7,37 @@
         <a :href="url" class="flex h-full items-center text-xl font-semibold">{{
           company
         }}</a>
+
+        <nav class="ml-12 h-full font-semibold">
+          <ul class="flex h-full list-none">
+            <li class="h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Teams</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Location</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5"
+                >Life at Hao Corp</a
+              >
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5"
+                >How we hire</a
+              >
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Students</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Jobs</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
