58996962571db5cb702071849d45d3ce68ce85f1
Hao Chen <mmkrty@gmail.com>
2023-10-06 14:51:44 +0200

    add profile image component

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,14 @@
+<template>
+  <img class="h-8 w-8 rounded-3xl object-contain" :src="imageLink" />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink: "../../public/Ruby_logo..png",
+    };
+  },
+};
+</script>
