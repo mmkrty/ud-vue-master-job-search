40981de4fe7731749e23e5825c6acef85316fc7d
Hao Chen <mmkrty@gmail.com>
2024-05-25 21:13:21 +0200

    initiate headline component, use life cycle hook

---
+++ src/components/TheHeadline.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>Build for everyone</h1>
+    <h2>Find your next job at Hao Careers</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheHeadline",
+  data() {
+    return {
+      sample: "This is a sample",
+    };
+  },
+  created() {
+    console.log("TheHeadline created, look: ", this.sample);
+  },
+};
+</script>
--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -4,7 +4,9 @@
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1 border-blue-700">1</div>
 
-        <div class="col-span-5 col-start-2 border-blue-700">5</div>
+        <div class="col-span-5 col-start-2 border-blue-700">
+          <the-headline />
+        </div>
 
         <div class="col-span-5 col-start-7 border-blue-700">5</div>
 
@@ -15,7 +17,12 @@
 </template>
 
 <script>
+import TheHeadline from "./TheHeadLine.vue";
+
 export default {
   name: "TheHero",
+  components: {
+    TheHeadline,
+  },
 };
 </script>
