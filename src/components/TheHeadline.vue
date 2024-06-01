b3f147cbcd85b5bc549b1693e6a4c43789f30679
Hao Chen <mmkrty@gmail.com>
2024-06-01 16:31:31 +0200

    use setInterval with life cycle hooks to change action text in Headline component

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <h1>Build for everyone</h1>
+    <h1>{{ action }} for everyone</h1>
     <h2>Find your next job at Hao Careers</h2>
   </section>
 </template>
@@ -10,11 +10,26 @@ export default {
   name: "TheHeadline",
   data() {
     return {
-      sample: "This is a sample",
+      action: "Build",
+      interval: null,
     };
   },
   created() {
-    console.log("TheHeadline created, look: ", this.sample);
+    this.changeTitle();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ["Build", "Create", "Design", "Code"];
+        const currentActionIndex = actions.indexOf(this.action);
+        const nextActionIndex = (currentActionIndex + 1) % actions.length;
+        const nextActon = actions[nextActionIndex];
+        this.action = nextActon;
+      }, 3000);
+    },
   },
 };
 </script>
