55ec1787cae4e6462b4c83c0fadea1c542af2603
Hao Chen <mmkrty@gmail.com>
2024-06-08 16:03:01 +0200

    extract next element function

---
+++ tests/utils/nextElementInList.test.js
@@ -0,0 +1,20 @@
+import { describe, expect } from "vitest";
+import nextElementInList from "../../src/utils/nextElemenyInList";
+
+describe("nextElementInList", () => {
+  it("locates elements in list and returns the next element in list", () => {
+    const list = ["A", "B", "C", "D", "E"];
+    const value = "C";
+    const result = nextElementInList(list, value);
+    expect(result).toBe("D");
+  });
+
+  describe("when element is at the end of the list", () => {
+    it("locates next element at the start of the list", () => {
+      const list = ["A", "B", "C", "D", "E"];
+      const value = "E";
+      const result = nextElementInList(list, value);
+      expect(result).toBe("A");
+    });
+  });
+});
