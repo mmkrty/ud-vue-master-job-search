61ae88c283ee18f268a3d393c02a9a01aaba9b33
Hao Chen <mmkrty@gmail.com>
2025-01-14 20:51:02 +0100

    use v-model for binding text inputs

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,11 +6,7 @@
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute -top-10 left-0">Role</label>
-        <text-input
-          placeholder="Software engineer"
-          :value="role"
-          @handle-input="role = $event"
-        />
+        <text-input v-model="role" placeholder="Software engineer" />
       </div>
       <span
         class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3"
@@ -19,11 +15,7 @@
 
       <div class="relative flex h-full flex-1 items-center pl-3">
         <label class="absolute -top-10 left-0">Where?</label>
-        <text-input
-          placeholder="Los Angeles"
-          :value="location"
-          @handle-input="location = $event"
-        />
+        <text-input v-model="location" placeholder="Los Angeles" />
       </div>
     </div>
 
