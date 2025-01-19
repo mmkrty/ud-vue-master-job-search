d19a9489bef993de28a3003e19a5e7cda37c9e84
Hao Chen <mmkrty@gmail.com>
2025-01-19 20:26:34 +0100

    add main nav to app vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/jobs/results">Job Results</router-link>
-  </div>
+  <main-nav />
   <router-view />
 </template>
 
 <script>
+import MainNav from "./components/Navigation/MainNav.vue";
+
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>
