8f1f30995de322fd2d0645bd98e7b02d4d78efe9
Hao Chen <mmkrty@gmail.com>
2025-01-20 22:44:27 +0100

    accessing

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,9 +1,16 @@
 <template>
   <h1 class="text-6xl font-bold">Job Results page</h1>
+  <h2>{{ experiment }}</h2>
 </template>
 
 <script>
 export default {
   name: "JobResultsView",
+  computed: {
+    experiment() {
+      console.log(this.$route);
+      return "experiment";
+    },
+  },
 };
 </script>
