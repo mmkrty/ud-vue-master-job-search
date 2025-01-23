4cc665b5c9076cb3e4459e2aecd72b85dce30ef6
Hao Chen <mmkrty@gmail.com>
2025-01-23 17:19:18 +0100

    navigate with  push method

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,15 +1,14 @@
 <template>
   <h1 class="text-6xl font-bold">Job Results page</h1>
-  <h2>{{ experiment }}</h2>
+  <button @click="goToHome">Home</button>
 </template>
 
 <script>
 export default {
   name: "JobResultsView",
-  computed: {
-    experiment() {
-      console.log(this.$router);
-      return "experiment";
+  methods: {
+    goToHome() {
+      this.$router.push({ name: "Home" });
     },
   },
 };
