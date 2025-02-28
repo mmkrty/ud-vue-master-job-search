392d6a45448c4e605339fc32d777bef267ca3de5
Hao Chen <mmkrty@gmail.com>
2025-02-28 22:06:50 +0100

    create job listings component

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <job-filters-side-bar />
+    <job-listings />
   </div>
 </template>
 
 <script>
 import JobFiltersSideBar from "@/components/JobResults/JobFiltersSidebar/JobFiltersSideBar.vue";
+import JobListings from "@/components/JobResults/JobListings.vue";
 
 export default {
   name: "JobResultsView",
   components: {
     JobFiltersSideBar,
+    JobListings,
   },
 };
 </script>
